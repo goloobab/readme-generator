cd6397b6add7a7b49403a263904ffd8ec836daf2
Umahoro Ntagana <goloobab@gmail.com>
2023-01-23 13:25:32 +0000

    Added questions to the js file

--- index.js
+++ index.js
@@ -4,9 +4,54 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?'
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should the project have?',
+    choices: ['APACHE 2.0','CCO', 'GNU v3', 'MIT', 'No License']
+},
+{
+    type: 'editor',
+    name: 'description',
+    message: 'Please write a description about your project'
+},
 
-];
+{
+    type: 'input',
+    name: 'installation',
+    message: 'What installation command is needed for the project?'
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'What is the usage requirement to use your project?'
+},
+
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'What is needed to contribute to your project?'
+},
+{
+    type: 'input',
+    name: 'test',
+    message: 'What commands do you use to run tests in this the project?'
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'What is your github username?'
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+}];
 
 // function to write README file
 function writeToFile(fileName, data) {
