bbf1f3bd4ac8a9fb8e94e6b9dbb7741948350689
Umahoro Ntagana <goloobab@gmail.com>
2023-01-23 13:44:43 +0000

    Added license URL for the bagdes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -44,4 +44,16 @@ function generateMarkdown(data) {
 `;
 }
 
+function getLicense(license) {
+  const licenses = new Map([
+    ["APACHE 2.0", "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"],
+    ["CCO", "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)"],
+    ["GNU v3", "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"],
+    ["MIT", "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"],
+    ["No License", "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"]
+  ])
+  
+  return licenses.get(license)
+}
+
 module.exports = generateMarkdown;
